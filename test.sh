0721b99740f8d4ebd9144e8d8ca0554483cd02f5
Bailey Marlow <Baileymarlow2@gmail.com>
2022-04-14 15:40:09 -0400

    updated test

--- test.sh
+++ test.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+check_sort=0
 # Test Normal Sort
 python3 CSC499HW1.py SortMe.txt normal > normsort.txt
 if diff NormalSorted.txt normsort.txt; then
     echo "Normal sort pass"
 else
     echo "Normal sort fail"
+    check_sort=1
+    exit $check_sort
 fi 
 
 #Test reverse sort
@@ -14,4 +17,7 @@ if diff ReverseSorted.txt revsort.txt; then
     echo "Reversed sort pass"
 else
     echo "Reversed sort fail"
+    check_sort=2
+    exit $check_sort
 fi 
+
